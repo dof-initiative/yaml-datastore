0d0f371adbaecdf7abb9e559833be04e42a2e206
Sean Marquez <capsulecorplab@gmail.com>
2026-02-10 23:05:13 +0000

    add TODO notes for additional test cases

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -21,8 +21,23 @@ describe("Test file name to complex string key name mapping function", () => {
   });
 });
 
+// TODO: test to ignore leading underscores
+// TODO: test to ignore trailing underscores
+// TODO: test to ignore leading and trailing underscores when both present
+// TODO: test to ignore non-single underscores
+// TODO: test to replace single underscores with dots
 describe("Test complex string key name to file name mapping function", () => {
   it("should do a thing", () => {
     //TODO
+  });
+});
+
+// TODO: test fileNameToComplexStringKey(complexStringKeyToFileName('_3mTape_md')) === '_3mTape_md'
+// TODO: test complexStringKeyToFileName(fileNameToComplexStringKey('_3mTape.md')) === '_3mTape.md'
+// TODO: 5 or so test cases of one-to-one and onto mapping
+describe("Test one-to-one and onto mapping", () => {
+  it("should do a thing", () => {
+    //TODO
+    //shall assert both mapping directions; fileNameToComplexStringKey() and complexStringKeyToFileName()
   });
 });
