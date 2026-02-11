bf37406c6c89856cabbca80fd003f8963e000933
Alexandria Little <alexa@alexalittle.me>
2026-02-11 13:25:04 +0000

    Add test cases to Test one-to-one and onto mapping

--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -87,8 +87,40 @@ describe("Test complex string key name to file name mapping function", () => {
 // TODO: test complexStringKeyToFileName(fileNameToComplexStringKey('_3mTape.md')) === '_3mTape.md'
 // TODO: 5 or so test cases of one-to-one and onto mapping
 describe("Test one-to-one and onto mapping", () => {
-  it("should do a thing", () => {
-    //TODO
-    //shall assert both mapping directions; fileNameToComplexStringKey() and complexStringKeyToFileName()
+  it("shall map keyName to keyName", () => {
+    const keyName = '3mTape_md';
+    const result = fileNameToComplexStringKey(complexStringKeyToFileName(keyName))
+
+    expect(result).to.equal(keyName);
+  });
+  it("shall map fileName to fileName", () => {
+    const fileName = '3mTape.md';
+    const result = complexStringKeyToFileName(fileNameToComplexStringKey(fileName))
+
+    expect(result).to.equal(fileName);
+  });
+  it("shall map keyName to keyName with leading and muliple underscores", () => {
+    const keyName = '_3m__tape_md';
+    const result = fileNameToComplexStringKey(complexStringKeyToFileName(keyName))
+
+    expect(result).to.equal(keyName);
+  });
+  it("shall map fileName to fileName with leading and multiple underscores", () => {
+    const fileName = '_3m__tape.md';
+    const result = complexStringKeyToFileName(fileNameToComplexStringKey(fileName))
+
+    expect(result).to.equal(fileName);
+  });
+  it("shall map keyName to keyName with multiple single underscores", () => {
+    const keyName = 'myView_md_njk';
+    const result = fileNameToComplexStringKey(complexStringKeyToFileName(keyName))
+
+    expect(result).to.equal(keyName);
+  });
+  it("shall map fileName to fileName with multiple single underscores", () => {
+    const fileName = 'myView.md.njk';
+    const result = complexStringKeyToFileName(fileNameToComplexStringKey(fileName))
+
+    expect(result).to.equal(fileName);
   });
 });
